a4c15622c863d1fc7150b30ceb898ac6545958ba
01-134181-021 <01-134181-021@student.bahria.edu.pk>
2024-09-03 18:30:42 +0500

    feat: chaanges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,29 @@ import Layout from "@theme/Layout";
 import FeaturesSection from "../components/HomepageFeatures/featuresSection";
 import HeroSection from "../components/HomepageFeatures/heroSection";
 import { useLocation } from "react-router-dom";
+import { useMemo } from "react";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   const location = useLocation();
-  const hideHamburger = location.pathname === "/";
+
+  const shouldHideHamburgerMenu = useMemo(
+    () => location.pathname === "/",
+    [location.pathname]
+  );
 
   return (
-    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <main>
-        {/* {hideHamburger && (
+        {shouldHideHamburgerMenu && (
           <style>
             {`
-            .navbar__toggle {
-              display: none !important;
-            }
-          `}
+              .navbar__toggle {
+                display: none !important;
+              }
+            `}
           </style>
-        )} */}
+        )}
         <HeroSection />
         <FeaturesSection />
       </main>
